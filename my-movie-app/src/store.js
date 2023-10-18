82f4de7e48654e6180745c9aade56fc7133ad5c7
vaishalipardeshi <80513781+vaishalipardeshi@users.noreply.github.com>
2023-10-18 12:11:36 +0530

    trying to add redux devtool ext.

--- my-movie-app/src/store.js
+++ my-movie-app/src/store.js
@@ -3,6 +3,10 @@ import {
 } from '@reduxjs/toolkit';
 
 import {
+  composeWithDevTools
+} from 'redux-devtools-extension';
+
+import {
   setupListeners
 } from '@reduxjs/toolkit/query';
 
@@ -14,13 +18,30 @@ import
   cacheReducer
   from '../src/featuresReducers/pokemonSlice';
 
+  // export const store = configureStore({
+  //   reducer: { 
+  //     [pokemonApi.reducerPath]: pokemonApi.reducer,
+  //     cache: cacheReducer, //add cache to store
+  //   },
+  //   middleware: (getDefaultMiddleware) => //handle api request
+  //   getDefaultMiddleware().concat(pokemonApi.middleware),
+  // });
+
+  //Add redux devtool extension.
+  const composeEnhancers = composeWithDevTools({
+    name: 'MyReduxStore',
+    actionsBlacklist: ['SOME_ACTION_TYPE'],
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+  });
+
   export const store = configureStore({
     reducer: { 
       [pokemonApi.reducerPath]: pokemonApi.reducer,
-      cache: cacheReducer, //add cache to store
+      cache: cacheReducer,
     },
     middleware: (getDefaultMiddleware) => //handle api request
     getDefaultMiddleware().concat(pokemonApi.middleware),
+    enhancer: [composeEnhancers],
   });
 
-  setupListeners(store.dispatch);// dispatch send actions to redux store.
+  setupListeners(store.dispatch);
