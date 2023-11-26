bfc3c946a8b3047088c348f965493dca80d326c3
vaishalipardeshi <80513781+vaishalipardeshi@users.noreply.github.com>
2023-11-26 22:43:14 +0530

    Update file

--- src/featuresReducers/pokemonSlice.js
+++ src/featuresReducers/pokemonSlice.js
@@ -5,13 +5,9 @@ import {
 import { useGetAllPokemonDetailsQuery } from '../services/api';
 
 const initialState = {
-  // allPokemon: {},
-  // pokemonDetails: {},
   favorites: [],
 };
 
-//slice function/part of store/based on slice req it will gives us the output.
-//for managing a cache in redux store
 const cacheSlice = createSlice({
   name: 'cache',
   initialState,
@@ -20,13 +16,10 @@ const cacheSlice = createSlice({
       const { key, data } = action.payload;
       state[key] = data;
     },
-    //to add an item to an array in state
-    //action.payload is data being added to favorites array. (hold data in redux action obj)
     addToFavorites: (state, action) => {
-      state.favorites.push(action.payload);//state.favorites is array in state & push(action.payload) is array used to add item to end of array
+      state.favorites.push(action.payload);
     },   
     // removeFromFavorites: (state, action) => {
-    //   // Filter out the Pokémon to remove by checking the ID
     //   state.favorites = state.favorites.filter(
     //     (pokemon) => pokemon.id !== action.payload.id
     //   );
@@ -40,7 +33,7 @@ export default cacheSlice.reducer;
 
 //fetch data and cache data ,id for identify data we want to fetch and cache
 export const fetchDataAndCache = (id) => async(dispatch, getState) =>{
-  const state = getState(); //getState allows you to access current state of redux state
+  const state = getState();
   const { cache } = state;
 
   //check if the data is in the redux store or not
@@ -48,7 +41,6 @@ export const fetchDataAndCache = (id) => async(dispatch, getState) =>{
     const response = await dispatch(useGetAllPokemonDetailsQuery(id));
     const pokemonData = response.data;
 
-    //cache data
     dispatch(setCacheValue({key: `pokemonDetails.${id}`, value: pokemonData}));
   }
 }
@@ -60,13 +52,12 @@ export const getDataFromCacheAndFilter = (filter) => (dispatch, getState) => {
 
   const { pokemonDetails } = cache;
 
-  const filteredData = {}; //stored filtered result
+  const filteredData = {}; 
 
-  //loop the cache & filter based on key-value pair
   for (const key in pokemonDetails) {
     const pokemon = pokemonDetails[key];
-    if(pokemon.someFilterCondCriteria === filter) { //here it matches the filtered criteria
-      filteredData[key] = pokemon; //if matches then adds key-value pair to filteredData obj
+    if(pokemon.someFilterCondCriteria === filter) {
+      filteredData[key] = pokemon;
     }
   }
   return filteredData;
